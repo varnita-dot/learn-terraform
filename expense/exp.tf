b43f5003589abc0f3449b7e1ea6f7da39a0519f3
rajeshfegade <rajeshfegade@gmail.com>
2023-10-31 14:59:17 +0530

    create ec2 by terraform

--- expense/exp.tf
+++ expense/exp.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "frontend" {
-  ami = data.aws_ami.ami_name.image_id
+  ami = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.security_group_name]
   tags = {
@@ -16,7 +16,7 @@ resource "aws_route53_record" "frontend" {
 }
 
 resource "aws_instance" "mysql" {
-  ami = data.aws_ami.ami_name.image_id
+  ami = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.security_group_name]
   tags = {
@@ -33,7 +33,7 @@ resource "aws_route53_record" "mysql" {
 }
 
 resource "aws_instance" "backend" {
-  ami = data.aws_ami.ami_name.image_id
+  ami = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.security_group_name]
   tags = {
--- expense/main.tf
+++ expense/main.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "ami_name" {
+data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["713669701478"]
