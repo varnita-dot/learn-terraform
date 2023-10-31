ab49f70ce68b9d2905227d9930f701b21e43d546
rajeshfegade <rajeshfegade@gmail.com>
2023-10-31 17:21:23 +0530

    create ec2 by terraform

--- expense/exp.tf
+++ expense/exp.tf
@@ -24,36 +24,56 @@ resource "aws_route53_record" "frontend" {
   records = [aws_instance.frontend.private_ip]
 }
 
-#resource "aws_instance" "mysql" {
-#  ami = data.aws_ami.example.image_id
-#  instance_type = "t3.micro"
-#  vpc_security_group_ids = [data.aws_security_group.sg.id]
-#  tags = {
-#    Name = "mysql"
-#  }
-#}
-#
-#resource "aws_route53_record" "mysql" {
-#  zone_id = data.aws_route53_zone.zone.zone_id
-#  name    = "mysql1.${var.zone_id}"
-#  type    = "A"
-#  ttl     = 300
-#  records = [aws_instance.mysql.private_ip]
-#}
-#
-#resource "aws_instance" "backend" {
-#  ami = data.aws_ami.example.image_id
-#  instance_type = "t3.micro"
-#  vpc_security_group_ids = [data.aws_security_group.sg.id]
-#  tags = {
-#    Name = "backend"
-#  }
-#}
-#
-#resource "aws_route53_record" "backend" {
-#  zone_id = data.aws_route53_zone.zone.zone_id
-#  name    = "backend1.${var.zone_id}"
-#  type    = "A"
-#  ttl     = 300
-#  records = [aws_instance.backend.private_ip]
-#}
+resource "aws_instance" "mysql" {
+  ami                    = data.aws_ami.example.image_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
+  tags                   = {
+    Name = "mysql"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/firstAnsibleProject
+git pull
+sleep
+ansible ${self.private_ip}, -e ansible_user=centos -e ansible_password = DevOps321 expense.yml -e service_name=mysql
+    EOF
+  }
+}
+
+
+resource "aws_route53_record" "mysql" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "mysql1.${var.zone_id}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mysql.private_ip]
+}
+
+resource "aws_instance" "backend" {
+  ami                    = data.aws_ami.example.image_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
+  tags                   = {
+    Name = "backend"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/firstAnsibleProject
+git pull
+sleep
+ansible ${self.private_ip}, -e ansible_user=centos -e ansible_password = DevOps321 expense.yml -e service_name=backend
+    EOF
+  }
+}
+
+
+resource "aws_route53_record" "backend" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "backend1.${var.zone_id}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.backend.private_ip]
+}
